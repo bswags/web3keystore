5f85c7ce20a75bbdd835512879e51a590d26860b
Brian Wagner <brian.wagner35@gmail.com>
2022-02-12 14:35:04 -0500

    Initial Commit

---
+++ Tests/web3keystoreTests/web3keystoreTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import web3keystore
+
+final class web3keystoreTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(web3keystore().text, "Hello, World!")
+    }
+}
